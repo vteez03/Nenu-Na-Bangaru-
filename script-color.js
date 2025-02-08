46edd990f11bd955e8dd9da46769d342de9d49cf
vteez03 <140069495+vteez03@users.noreply.github.com>
2025-02-08 13:00:27 +0530

    Update script-color.js

--- script-color.js
+++ script-color.js
@@ -1,15 +1,1 @@
-function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
 
-    noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
-
-    // Change button color each time it's clicked
-    const colors = ["#ff5722", "#ff9800", "#ffeb3b", "#03a9f4", "#9c27b0"];
-    noButton.style.backgroundColor = colors[messageIndex % colors.length];
-
-    // Increase Yes button size
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = '${currentSize * 1.5}px';
-}
